9646616e64c4cd20fba58a5c51d7e5b092fd7cf3
tranhoangdang <tranhoangdang1402@gmail.com>
2022-08-31 16:32:08 +0700

    commit

--- components/header.php
+++ components/header.php
@@ -1,10 +1,11 @@
 <?php
     if(isset($_COOKIE['news'])){
         $id = $_COOKIE['news'];
-        $sql = "SELECT id,avatar,is_active FROM users WHERE id = '$id'";
+        $sql = "SELECT id,avatar,name,is_active FROM users WHERE id = '$id'";
         $result = $conn->query($sql);
         $row = mysqli_fetch_assoc($result);
         $img = $row['avatar'];
+        $name = $row['name'];
         $status = $row['is_active'];
         if($status != 1){
             setcookie("login", '', time()-1);
@@ -26,16 +27,19 @@
             if(isset($_COOKIE['news'])){
                 $user = $_COOKIE['news'];
             ?>
-                <div class="dropdown">
-                    <div class="dropdown" data-bs-toggle="dropdown">
-                        <img src="public/uploads/<?= $img ?>" id="avatar-dropdown" alt="">
+                <div class="d-flex">
+                    <p><?= $name ?></p>
+                    <div class="dropdown">
+                        <div class="dropdown" data-bs-toggle="dropdown">
+                            <img src="public/uploads/<?= $img ?>" id="avatar-dropdown" alt="">
+                        </div>
+                        <ul class="dropdown-menu dropdown-menu-end">
+                            <li><a class="dropdown-item" href="profile.php?id=<?= $id ?>">Tài khoản</a></li>
+                            <li><a class="dropdown-item" href="my-article.php?id=<?= $id ?>">Bài viết đã thích</a></li>
+                            <li><a class="dropdown-item" href="change-password.php">Đổi mật khẩu</a></li>
+                            <li><a class="dropdown-item" href="logout.php">Đăng xuất</a></li>
+                        </ul>
                     </div>
-                    <ul class="dropdown-menu dropdown-menu-end">
-                        <li><a class="dropdown-item" href="profile.php?id=<?= $id ?>">Tài khoản</a></li>
-                        <li><a class="dropdown-item" href="my-article.php?id=<?= $id ?>">Bài viết đã thích</a></li>
-                        <li><a class="dropdown-item" href="change-password.php">Đổi mật khẩu</a></li>
-                        <li><a class="dropdown-item" href="logout.php">Đăng xuất</a></li>
-                    </ul>
                 </div>
             <?php
             }
