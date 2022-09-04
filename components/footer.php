135f3a5dfc23b98a19f80d132c9b381864bfc604
tranhoangdang <tranhoangdang1402@gmail.com>
2022-09-05 02:15:22 +0700

    commit

--- components/footer.php
+++ components/footer.php
@@ -1,11 +1,11 @@
 <div class="bg-dark mt-auto">
     <div class="container">
-        <footer class="d-flex flex-wrap justify-content-between align-items-center py-5">
+        <footer class="d-flex flex-wrap justify-content-between align-items-center py-3">
             <div class="col-md-6 col-12 d-flex align-items-center">
                 <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
                     <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
                 </a>
-                <span class="mb-3 mb-md-0 text-muted">© 2022 Ứng dụng được phát triển bởi Trần Hoàng Đăng</span>
+                <span class="mb-3 mb-md-0 text-white">© 2022 Ứng dụng được phát triển bởi Trần Hoàng Đăng</span>
             </div>
             <ul class="nav col-md-4 col-12 justify-content-end list-unstyled d-flex">
                 <li class="ms-3"><a class="text-white" href="https://github.com/hngdang" target="_blank"><i class="bi bi-github"></i></a></li>
